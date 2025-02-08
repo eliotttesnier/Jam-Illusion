d5660cac71dc19acd934ed4ec9652491ca47663b
eliotttesnier <eliott.tesnier@epitech.eu>
2025-02-08 01:13:32 +0100

    [ADD] Initilisation

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** Jam-Illusion
+** File description:
+** main
+*/
+
+#include "Game.hpp"
+
+int main()
+{
+	Game game;
+
+	return 0;
+}
---
+++ src/Game.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** Jam-Illusion
+** File description:
+** Game
+*/
+
+#pragma once
+
+class Game {
+    public:
+        Game();
+        ~Game();
+
+    protected:
+    private:
+};
